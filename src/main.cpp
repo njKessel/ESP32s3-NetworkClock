1fd63d7c9e97c540826c9dee84881ffff8b6b10c
Nathan Kessel <kesselnj@mail.uc.edu>
2025-11-04 17:58:13 -0500

    Built addDecimal, displayPattern array, displayIndex array, and displayTime which is incomplete.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include <Arduino.h> // Core types and functions for arduino 
-#include "secrets.h" // WiFi Cred
+#include "secrets.h" // WiFi Cred, change to secrets.example.h
 #include <WiFi.h>    // Router Connection
 #include "time.h"    // POSIX time + SNTP
 #include <Wire.h>
 
 const uint8_t I2C_Address = 0x70; // Scan agrees 10.26
-int iterator = 0x00;
 
 // SNTP sync
 void initTime(const char* tz) {
@@ -67,6 +66,62 @@ void displayClean(){
   }
 }
 
+int addDecimal(int numberVal) {
+  int increasedNumVal = numberVal | 0x80;
+  return increasedNumVal;
+}
+
+const int displayPattern[] = {
+  0x3F, // 0
+  0x06, // 1
+  0x5B, // 2
+  0x4F, // 3
+  0x66, // 4
+  0x6D, // 5
+  0x7d, // 6
+  0x07, // 7
+  0x7f, // 8 
+  0x67  // 9
+};
+
+const int displayIndex[] = { // Simple list of the hex values for transmission that correspond to digits
+  0x00, // CHAR 0
+  0x02, // CHAR 1
+  0x04, // CHAR 2
+  0x06, // CHAR 3
+  0x08, // CHAR 4
+  0x0A, // CHAR 5
+  0x0C, // CHAR 6
+  0x0E  // CHAR 7
+};
+
+void displayTime(char tbuf[]) {
+  bool decimalCarry = 1;
+  int displayIndexTracer = strlen(tbuf);
+  for(int displayIterator = strlen(tbuf); displayIterator >= 0; displayIterator--;) {
+    Wire.beginTransmission(I2C_Address);
+    if (tbuf[displayIterator] == ':') {
+            decimalCarry = 1;
+            displayIndexTracer--;
+            Wire.endTransmission();
+    } else {
+      if (decimalCarry == 0) {
+        Wire.write(displayIndex[displayIndexTracer]);
+        Wire.write(displayPattern[tbuf[displayIterator]]);
+        Wire.endTransmission();
+        
+      } else {
+        Wire.write(displayIndex[displayIndexTracer]);
+        Wire.write(addDecimal(displayPattern(tbuf[displayIterator])));
+        Wire.endTransmission();
+        displayIndexTracer--;
+        decimalCarry
+      }
+      displayIndexTracer--;
+      
+    }
+  }
+}
 
 
 void setup() {
@@ -86,26 +141,17 @@ void setup() {
 }
 
 void loop() {
-  Wire.beginTransmission(I2C_Address);
-    Wire.write(0x00);
-    Wire.write(0x5B);
-    Wire.endTransmission();
-  iterator++;
-  if (iterator == 0x0F) {
-    
-    iterator = 0x00;
+  struct tm ti;
+  if (getLocalTime(&ti)) {
+    char tbuf[9];
+    strftime(tbuf, sizeof(tbuf), "%H:%M:%S", &ti); // FORMATS TIME AS HH:MM:SS
+
+    char zbuf[17];
+    strftime(zbuf, sizeof(zbuf), "%Z %z", &ti);
+
+    int len = strlen(zbuf);
+    (void)len;
   }
+  displayTime(tbuf[]);
   delay(500);
-  // struct tm ti;
-  // if (getLocalTime(&ti)) {
-  //   char tbuf[9];
-  //   strftime(tbuf, sizeof(tbuf), "%H:%M:%S", &ti);
-
-  //   char zbuf[17];
-  //   strftime(zbuf, sizeof(zbuf), "%Z %z", &ti);
-
-  //   int len = strlen(zbuf);
-  //   (void)len; // silence unused warning for now
-  // }
-
 }
