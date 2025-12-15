c46a6be2c6e6b18794efefaddb963c5b22a9a463
njkessel <nathanielkessel@gmail.com>
2025-12-15 16:04:25 -0500

    Begin of SPI display logic

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ https://github.com/njKessel/ESP32s3-NetworkClock
 
 #include <Arduino.h>                // Core types and functions for arduino 
 #include <WiFi.h>                   // Router Connection
+#include <SPI.h>                    // SPI
 #include <Wire.h>                   // I2C
 #include <time.h>                   // POSIX time + SNTP
 
@@ -16,6 +17,18 @@ const uint8_t I2C_Address = 0x70; // Scan agrees 10.26
 const int TZDISPLAY = 21; // GPIO PIN 21
 int TZDISPLAY_status = 0;
 
+// DISPLAY PINS
+constexpr int PIN_COPI  = 11;  // SERIAL INPUT
+constexpr int PIN_LATCH = 40;  // RCLK
+constexpr int PIN_OE    = 4;   // BRIGHTNESS (PWM)
+constexpr int PIN_SCK   = 12;  // SCK
+
+SPISettings srSettings(10000000, MSBFIRST, SPI_MODE0);
+
+static void latchPulse() {
+  digitalWrite(PIN_LATCH, HIGH);
+  digitalWrite(PIN_LATCH, LOW);
+}
 // SNTP sync
 void initTime(const char* tz) {
   tm ti{}; // Zero initialization 
@@ -56,6 +69,7 @@ void setTimeFF(int yr, int month, int mday, int hr, int minute, int sec, int isD
   struct timeval now = { .tv_sec = t };
   settimeofday(&now, NULL);
 }
+
 void displayInitialize(){
   Wire.begin(4, 5); // keep I2C active for manual writes later
   Wire.beginTransmission(I2C_Address);
@@ -75,70 +89,84 @@ void displayClean(){
   }
 }
 
-const String TZ_menu[] = {                                    // TIME ZONE POSIX STRINGS, [POSIX, TIME ZONE, OFFSET, CITY]
-  ["AOE12", "AOE", "-12", "Baker Island"],                                              // 0 AOE
-  ["NUT11", "NUT", "-11", "American Samoa"],                                            // 1 NUT
-  ["HST11HDT,M3.2.0/2:00:00,M11.1.0/2:00:00", "HST", "-10", "Hawaii"],                  // 2 HST
-  ["MART9:30,M3.2.0/2:00:00,M11.1.0/2:00:00", "MART", "-9:30", "French Polynesia"],     // 3 MART
-
-  ["AKST9AKDT,M3.2.0/2:00:00,M11.1.0/2:00:00", "AKST", "-9", "Alaska"],                 // 4 AKST
-  ["PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00", "PDT", "-8", "Los Angeles"],               // 5 PDT
-  ["MST7MDT,M3.2.0/2:00:00,M11.1.0/2:00:00", "MST", "-7", "Denver"],                    // 6 MST/DST
-  ["MST7", "MST", "-7", "Phoenix"],                                                     // 7 MST
-  ["CST6CDT,M3.2.0/2:00:00,M11.1.0/2:00:00", "CST", "-6", "Chicago"],                   // 8 CST
-  ["EST5EDT,M3.2.0/2,M11.1.0/2", "EST", "-5", "New York"],                              // 9 EST
-
-  ["AST4ADT,M3.2.0/2:00:00,M11.1.0/2:00:00", "AST", "-4", "Halifax"],                   // 10 AST
-  ["BRT3", "BRT", "-3", "São Paulo"],                                                   // 11 BRT
-  ["FKST3FKDT,M9.1.0/2:00:00,M4.3.0/2:00:00", "FKST", "-3", "Falkland Islands"],        // 12 FKST
-  ["GRNL2", "GRL", "-2", "South Georgia"],                                              // 13 GRL
-
-  ["AZOT1AZOST,M3.5.0/0:00:00,M10.5.0/1:00:00", "AZOT", "-1", "Azores"],                // 14 AZOT
-  ["GMT0", "GMT", "0", "London"],                                                       // 15 GMT
-  ["GMT0BST,M3.5.0/1:00:00,M10.5.0/2:00:00", "BST", "+1", "United Kingdom"],            // 16 GMT/DST
-  ["CET-1CEST,M3.5.0/2:00:00,M10.5.0/3:00:00", "CET", "+1", "Berlin"],                  // 17 CET
-  ["WAT-1", "WAT", "+1", "Nigeria"],                                                    // 18 WAT
-  ["EET-2EEST,M3.5.0/3:00:00,M10.5.0/4:00:00", "EET", "+2", "Athens"],   
-
-  ["CAT-2", "CAT", "+2", "South Africa"],                                               // 19 CAT
-  ["MSK-3", "MSK", "+3", "Moscow"],                                                     // 20 MSK
-  ["AST-3", "AST", "+3", "Saudi Arabia"],                                               // 21 AST
-
-  ["GST-4", "GST", "+4", "Dubai"],                                                      // 22 GST
-  ["AZT-4AZST,M3.5.0/4:00:00,M10.5.0/5:00:00", "AZT", "+4", "Azerbaijan"],              // 23 AZT
-  ["AFT-4:30", "AFT", "+4:30", "Afghanistan"],                                          // 24 AFT
-  ["PKT-5", "PKT", "+5", "Pakistan"],                                                   // 25 PKT
-  ["IST-5:30", "IST", "+5:30", "India"],                                                // 26 IST
-  ["NPT-5:45", "NPT", "+5:45", "Nepal"],                                                // 27 NPT
-
-  ["BST-6", "BST", "+6", "Bangladesh"],                                                 // 28 BST
-  ["MMT-6:30", "MMT", "+6:30", "Myanmar"],                                              // 29 MMT
-  ["ICT-7", "ICT", "+7", "Thailand"],                                                   // 30 ICT
-  ["WIB-7", "WIB", "+7", "Indonesia West"],                                             // 31 WIB
-
-  ["CST-8", "CST", "+8", "China"],                                                      // 32 CST
-  ["AWST-8", "AWST", "+8", "Perth"],                                                    // 33 AWST
-  ["HKT-8", "HKT", "+8", "Hong Kong"],                                                  // 34 HKT
-  ["WITA-8", "WITA", "+8", "Indonesia Central"],                                        // 35 WITA
-
-  ["JST-9", "JST", "+9", "Japan"],                                                      // 36 JST
-  ["KST-9", "KST", "+9", "South Korea"],                                                // 37 KST
-  ["ACST-9:30ACDT,M10.1.0/2:00:00,M4.1.0/3:00:00", "ACST", "+9:30", "Adelaide"],        // 38 ACST/ACDT
-  ["CST-9:30", "CST", "+9:30", "Northern Territory"],                                   // 39 ACST
-
-  ["AEST-10AEDT,M10.1.0/2:00:00,M4.1.0/3:00:00", "AEST", "+10", "Sydney"],              // 40 AEDT
-  ["CHUT-10", "CHUT", "+10", "Chuuk"],                                                  // 41 CHUT
-  ["WIT-9", "WIT", "+9", "Indonesia East"],                                             // 42 WIT
-
-  ["SBT-11", "SBT", "+11", "Solomon Islands"],                                          // 43 SBT
-  ["NCT-11", "NCT", "+11", "New Caledonia"],                                            // 44 NCT
-  ["NZST-12NZDT,M9.5.0/2:00:00,M4.1.0/3:00:00", "NZST", "+12", "New Zealand"],          // 45 NZST
-  ["FJT-12FJDT,M11.1.0/2:00:00,M1.2.0/3:00:00", "FJT", "+12", "Fiji"],                  // 46 FJT
-
-  ["TOT-13", "TOT", "+13", "Tonga"],                                                    // 47 TOT
-  ["PHOT-13", "PHOT", "+13", "Phoenix Islands"],                                        // 48 PHOT
-  ["LINT-14", "LINT", "+14", "Line Islands"]                                            // 49 LINT
+struct TZEntry {
+  const char* posix;   // POSIX TZ string
+  const char* tz;      // short label for menu
+  const char* offset;  // human-readable offset
+  const char* city;    // example city/region
 };
+
+const TZEntry TZ_menu[] = {
+  {"AOE12", "AOE", "-12", "Baker Island"},                                              // 0
+  {"NUT11", "NUT", "-11", "American Samoa"},                                            // 1
+
+  // Hawaii does NOT use DST; POSIX offset for UTC-10 is "HST10"
+  {"HST10", "HST", "-10", "Hawaii"},                                                    // 2
+
+  // Marquesas Islands (UTC-9:30) do NOT use DST
+  {"MART9:30", "MART", "-9:30", "French Polynesia"},                                    // 3
+
+  {"AKST9AKDT,M3.2.0/2:00:00,M11.1.0/2:00:00", "AKST", "-9", "Alaska"},                 // 4
+  {"PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00", "PDT", "-8", "Los Angeles"},              // 5
+  {"MST7MDT,M3.2.0/2:00:00,M11.1.0/2:00:00", "MST", "-7", "Denver"},                   // 6
+  {"MST7", "MST", "-7", "Phoenix"},                                                    // 7
+  {"CST6CDT,M3.2.0/2:00:00,M11.1.0/2:00:00", "CST", "-6", "Chicago"},                  // 8
+  {"EST5EDT,M3.2.0/2,M11.1.0/2", "EST", "-5", "New York"},                             // 9
+
+  {"AST4ADT,M3.2.0/2:00:00,M11.1.0/2:00:00", "AST", "-4", "Halifax"},                  // 10
+  {"BRT3", "BRT", "-3", "São Paulo"},                                                  // 11
+  {"FKST3FKDT,M9.1.0/2:00:00,M4.3.0/2:00:00", "FKST", "-3", "Falkland Islands"},       // 12
+  {"GRNL2", "GRL", "-2", "South Georgia"},                                             // 13
+
+  {"AZOT1AZOST,M3.5.0/0:00:00,M10.5.0/1:00:00", "AZOT", "-1", "Azores"},               // 14
+  {"GMT0", "GMT", "0", "London"},                                                      // 15
+  {"GMT0BST,M3.5.0/1:00:00,M10.5.0/2:00:00", "BST", "+1", "United Kingdom"},           // 16
+  {"CET-1CEST,M3.5.0/2:00:00,M10.5.0/3:00:00", "CET", "+1", "Berlin"},                 // 17
+  {"WAT-1", "WAT", "+1", "Nigeria"},                                                   // 18
+  {"EET-2EEST,M3.5.0/3:00:00,M10.5.0/4:00:00", "EET", "+2", "Athens"},                 // 19
+
+  {"CAT-2", "CAT", "+2", "South Africa"},                                              // 20
+  {"MSK-3", "MSK", "+3", "Moscow"},                                                    // 21
+  {"AST-3", "AST", "+3", "Saudi Arabia"},                                              // 22
+
+  {"GST-4", "GST", "+4", "Dubai"},                                                     // 23
+  {"AZT-4AZST,M3.5.0/4:00:00,M10.5.0/5:00:00", "AZT", "+4", "Azerbaijan"},             // 24
+  {"AFT-4:30", "AFT", "+4:30", "Afghanistan"},                                         // 25
+  {"PKT-5", "PKT", "+5", "Pakistan"},                                                  // 26
+  {"IST-5:30", "IST", "+5:30", "India"},                                               // 27
+  {"NPT-5:45", "NPT", "+5:45", "Nepal"},                                               // 28
+
+  {"BST-6", "BST", "+6", "Bangladesh"},                                                // 29
+  {"MMT-6:30", "MMT", "+6:30", "Myanmar"},                                             // 30
+  {"ICT-7", "ICT", "+7", "Thailand"},                                                  // 31
+  {"WIB-7", "WIB", "+7", "Indonesia West"},                                            // 32
+
+  {"CST-8", "CST", "+8", "China"},                                                     // 33
+  {"AWST-8", "AWST", "+8", "Perth"},                                                   // 34
+  {"HKT-8", "HKT", "+8", "Hong Kong"},                                                 // 35
+  {"WITA-8", "WITA", "+8", "Indonesia Central"},                                       // 36
+
+  {"JST-9", "JST", "+9", "Japan"},                                                     // 37
+  {"KST-9", "KST", "+9", "South Korea"},                                               // 38
+  {"ACST-9:30ACDT,M10.1.0/2:00:00,M4.1.0/3:00:00", "ACST", "+9:30", "Adelaide"},       // 39
+  {"CST-9:30", "CST", "+9:30", "Northern Territory"},                                  // 40
+
+  {"AEST-10AEDT,M10.1.0/2:00:00,M4.1.0/3:00:00", "AEST", "+10", "Sydney"},             // 41
+  {"CHUT-10", "CHUT", "+10", "Chuuk"},                                                 // 42
+  {"WIT-9", "WIT", "+9", "Indonesia East"},                                            // 43
+
+  {"SBT-11", "SBT", "+11", "Solomon Islands"},                                         // 44
+  {"NCT-11", "NCT", "+11", "New Caledonia"},                                           // 45
+  {"NZST-12NZDT,M9.5.0/2:00:00,M4.1.0/3:00:00", "NZST", "+12", "New Zealand"},         // 46
+  {"FJT-12FJDT,M11.1.0/2:00:00,M1.2.0/3:00:00", "FJT", "+12", "Fiji"},                 // 47
+
+  {"TOT-13", "TOT", "+13", "Tonga"},                                                   // 48
+  {"PHOT-13", "PHOT", "+13", "Phoenix Islands"},                                       // 49
+  {"LINT-14", "LINT", "+14", "Line Islands"}                                           // 50
+};
+
+const size_t TZ_MENU_COUNT = sizeof(TZ_menu) / sizeof(TZ_menu[0]);
+
 
 enum anodeBit : uint32_t {
   ANODE0 = 1u << 25,  // 74HC595 U4 pin ANODE1 on 12C-LTP-587HR_rev1
