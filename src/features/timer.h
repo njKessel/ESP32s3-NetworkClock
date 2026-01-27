440ba89300bd851b2ebef99105f65e409d4d7a6a
njkessel <nathanielkessel@gmail.com>
2026-01-27 12:27:09 -0500

    timer class logic

--- src/features/timer.h
+++ src/features/timer.h
@@ -3,6 +3,19 @@
 
 #include <Arduino.h>
 
+struct timerData {
+    int timerID;
+    uint8_t timerHours;
+    uint8_t timerMinute;
+};
 
+class timer {
+  private:
+    timerData table[3];
+
+    unsigned long startTime;
+    unsigned long timeElapsed;
+    
+};
 
 #endif
