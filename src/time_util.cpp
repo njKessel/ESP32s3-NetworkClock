2142fff387c1eec1cd8371a374c01ff6dea67700
njkessel <nathanielkessel@gmail.com>
2026-02-24 10:45:33 -0500

    moved time functions to time util

---
+++ src/time_util.cpp
@@ -0,0 +1,12 @@
+#include "time_util.h"
+
+void initTime(const char* tz) {
+    configTzTime(tz, "pool.ntp.org");  
+}
+
+String formatTime(const tm& ti, bool hour24) {
+  char buf[16];                                                                 // INIT DISPLAY CHAR BUFFER
+  if (!hour24) strftime(buf, sizeof(buf), " %I:%M:%S  %p ", &ti);               // WRITE TEXT TO DISPLAY TO CHAR BUFFER FORMATTED FOR 12HR
+  else strftime(buf, sizeof(buf), "   %H:%M:%S   ", &ti);                       // WRITE TEXT TO DISPLAY TO CHAR BUFFER FORMATTER FOR 24HR
+  return String(buf);                                                           // RETURN THE TEXT TO DISPLAY AS A STRING
+}
